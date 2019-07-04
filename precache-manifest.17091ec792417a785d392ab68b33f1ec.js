afb708a7dde582ec2217bb0ded6ef1db592f3fe3
romanyanke <roman@yanke.ru>
2019-07-04 21:21:41 +0300

    Updates

---
+++ precache-manifest.17091ec792417a785d392ab68b33f1ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e44a3952604f7246f9d863dc04cb79a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b0d944cc879cffd653c",
+    "url": "/static/css/main.d03f7fae.chunk.css"
+  },
+  {
+    "revision": "6de255b8bb37f8222725",
+    "url": "/static/js/2.212b3d12.chunk.js"
+  },
+  {
+    "revision": "9b0d944cc879cffd653c",
+    "url": "/static/js/main.1b77a27d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
