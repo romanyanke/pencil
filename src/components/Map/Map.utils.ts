a15772dd37ea8be4c1136d4c2d252b8294cf853e
romanyanke <roman@yanke.ru>
2019-09-14 21:17:46 +0300

    map style

--- src/components/Map/Map.utils.ts
+++ src/components/Map/Map.utils.ts
@@ -1,7 +1,7 @@
 import { GeographyProps } from 'react-simple-maps'
 import { MapGeoStyleProps } from './Map.interface'
 
-const lightGrey = '#eceff1'
+const white = '#fff'
 const blue = '#309cd0'
 const darkBlue = '#226a8c'
 const red = '#ff5722'
@@ -11,13 +11,13 @@ export const mapGeoIdToStyle = ({
   isSelected,
 }: MapGeoStyleProps): GeographyProps['style'] => ({
   default: {
-    fill: isSelected ? red : hasPencil ? blue : lightGrey,
+    fill: isSelected ? red : hasPencil ? blue : white,
     outline: 'none',
   },
   hover: {
-    fill: isSelected ? red : hasPencil ? darkBlue : lightGrey,
+    fill: isSelected ? red : hasPencil ? darkBlue : white,
     cursor: hasPencil ? 'pointer' : 'default',
     outline: 'none',
   },
-  pressed: { fill: isSelected ? red : hasPencil ? darkBlue : lightGrey, outline: 'none' },
+  pressed: { fill: isSelected ? red : hasPencil ? darkBlue : white, outline: 'none' },
 })
