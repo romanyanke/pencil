462d0905af6e0058fad047500d9b11530799af4e
romanyanke <roman@yanke.ru>
2019-12-18 16:10:12 +0300

    --wip-- [skip ci]

--- src/components/PencilInfo/Siblings/Siblings.tsx
+++ src/components/PencilInfo/Siblings/Siblings.tsx
@@ -1,11 +1,12 @@
 import classNames from 'classnames'
 import React from 'react'
-import { useSiblings } from '../../Filter/Filter.hooks'
+import { useFilter, useSiblings } from '../../Filter/Filter.hooks'
 import { mapFilterToQueryString } from '../../Filter/Filter.utils'
 import { SiblingsProps } from './Siblings.interface'
 
 const Siblings = ({ pencilId }: SiblingsProps) => {
   const siblings = useSiblings(pencilId)
+  const [, setFilter] = useFilter()
 
   return (
     <div className="Siblings">
@@ -15,7 +16,13 @@ const Siblings = ({ pencilId }: SiblingsProps) => {
           className={classNames('Siblings-sibling', pencil && 'Siblings-active')}
         >
           {pencil && (
-            <a href={mapFilterToQueryString({ display: pencil.id })}>
+            <a
+              href={mapFilterToQueryString({ display: pencil.id })}
+              onClick={e => {
+                e.preventDefault()
+                setFilter({ display: pencil.id })
+              }}
+            >
               <img src={pencil.preview} alt={pencil.title} />
             </a>
           )}
