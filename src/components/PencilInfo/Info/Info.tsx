171f100321a7735298f5485438b5b44249cd31cb
romanyanke <roman@yanke.ru>
2021-01-03 10:36:27 +0300

    add thumbs

--- src/components/PencilInfo/Info/Info.tsx
+++ src/components/PencilInfo/Info/Info.tsx
@@ -1,5 +1,7 @@
-import { useRef, useEffect } from 'react'
 import { FormattedMessage } from 'react-intl'
+import { useEffect, useRef, useState } from 'react'
+import { debounce } from 'lodash'
+import classNames from 'classnames'
 import { usePseudoClick } from './Info.hooks'
 import { InfoProps } from './Info.interface'
 import messages from './Info.messages'
@@ -7,36 +9,65 @@ import { mapFilterToQueryString } from '../../Filter/Filter.utils'
 
 const Info = ({ pencil }: InfoProps) => {
   const handlePseudoLink = usePseudoClick()
-  const { country, city } = pencil
-  const scroller = useRef<HTMLDivElement>(null)
+  const { country, city, photos } = pencil
+  const scroller = useRef<HTMLDivElement | null>(null)
+  const [highlighIndex, setHiglightedIndex] = useState(0)
+
+  const getThumbClassName = (index: number) =>
+    classNames('Info-thumb', {
+      'Info-thumb-highlight': index === highlighIndex,
+      'Info-thumb-info': photos.length === index,
+    })
 
   useEffect(() => {
-    if (scroller.current) {
-      scroller.current.scrollTo({ top: scroller.current.clientHeight })
-    }
-  }, [pencil.id])
+    const el = scroller.current
+    if (el) {
+      const onScrollChange = debounce(() => {
+        requestAnimationFrame(() => {
+          if (el) {
+            setHiglightedIndex(Math.round(el.scrollTop / el.clientHeight))
+          }
+        })
+      }, 16)
 
-  const scrollInfo = () => {
-    if (scroller.current) {
-      scroller.current.scrollTo({ top: 0, behavior: 'smooth' })
+      el.addEventListener('scroll', onScrollChange)
+
+      return () => {
+        el.removeEventListener('scroll', onScrollChange)
+      }
     }
-  }
+  }, [])
 
   return (
     <div className="Info">
-      <div className="Info-scroller" ref={scroller}>
+      <div className="Info-thumbs" onClick={e => e.stopPropagation()}>
+        {photos.map((src, index) => {
+          const idAttr = `#thumb${index}`
+
+          return (
+            <a
+              className={getThumbClassName(index)}
+              key={src}
+              href={idAttr}
+              style={{ backgroundImage: `url(${src})` }}
+            />
+          )
+        })}
         {pencil.country && (
-          <button
-            onClick={e => {
-              e.stopPropagation()
-              scrollInfo()
-            }}
-            className="Info-country"
-          >
-            {pencil.country.flag}
-          </button>
+          <a className={getThumbClassName(photos.length)} href="#info" title={pencil.country.name}>
+            <span>{pencil.country.flag}</span>
+          </a>
         )}
-        <div className="Info-frame Info-content" onClick={handlePseudoLink}>
+      </div>
+
+      <div className="Info-scroller" ref={scroller}>
+        {pencil.photos.map((src, index) => (
+          <div key={src} className="Info-frame" id={`thumb${index}`}>
+            <img className="Info-photo" alt={pencil.title} src={src} />
+          </div>
+        ))}
+
+        <div className="Info-frame Info-content" id="info" onClick={handlePseudoLink}>
           <h1>{pencil.title}</h1>
 
           <b>
@@ -67,12 +98,6 @@ const Info = ({ pencil }: InfoProps) => {
             ))}
           </p>
         </div>
-
-        {pencil.photos.map(src => (
-          <div key={src} className="Info-frame">
-            <img className="Info-photo" alt={pencil.title} src={src} />
-          </div>
-        ))}
       </div>
     </div>
   )
