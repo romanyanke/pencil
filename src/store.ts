480321a32ce2e0d20af49d2e2364528d0d79502f
romanyanke <roman@yanke.ru>
2022-05-14 16:52:17 +0300

    fix RTK middleware

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { api } from './api'
 
-export const store = configureStore({ reducer: { [api.reducerPath]: api.reducer } })
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(api.middleware),
+})
