2c5a85ccc895131bc11c8059059ae90894a64048
romanyanke <roman@yanke.ru>
2022-05-14 14:00:21 +0300

    update codebase

--- src/Feed/Feed.tsx
+++ src/Feed/Feed.tsx
@@ -1,9 +1,9 @@
-import { createContext, FC } from 'react'
+import { createContext, PropsWithChildren } from 'react'
 import { useFeedContextValue } from './Feed.hooks'
 
 export const FeedContext = createContext<ReturnType<typeof useFeedContextValue>>(null as any)
 
-const Feed: FC = ({ children }) => {
+const Feed = ({ children }: PropsWithChildren<{}>) => {
   const value = useFeedContextValue()
 
   return <FeedContext.Provider value={value}>{children}</FeedContext.Provider>
